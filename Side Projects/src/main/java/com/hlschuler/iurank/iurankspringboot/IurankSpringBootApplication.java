58ff10c9d3c1badd919caa91814103cf744fb232
Hunter Schuler <hschuler2992@gmail.com>
2020-11-15 11:56:50 -0600

    IsIndianaFootballRanked.com Initial Upload

---
+++ Side Projects/src/main/java/com/hlschuler/iurank/iurankspringboot/IurankSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.hlschuler.iurank.iurankspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IurankSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IurankSpringBootApplication.class, args);
+	}
+
+}
